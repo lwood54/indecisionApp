dbcf34370b42dc7efdedc1e7ab2cc371c6a23561
Logan Wood <lwood3499@gmail.com>
2018-01-22 22:03:54 -0600

    setup Babel and presets env,react

---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log('App.js is running!');
+
+// JSX - JavaScript XML
+var template = <h1>Changed again?</h1>;
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);

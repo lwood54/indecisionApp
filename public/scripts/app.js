7b575e387aa3f3cc6af15720133b2b89fc945af3
Logan Wood <lwood3499@gmail.com>
2018-01-23 21:53:55 -0600

    using template ternary and && tricks

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,94 @@
-"use strict";
+'use strict';
 
+// JSX file that will auto-generate content via babel and output to: public/scripts/app.js
+// command line: babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
 console.log('App.js is running!');
 
-// JSX - JavaScript XML
-var template = React.createElement(
-  "h1",
-  null,
-  "Changed again?"
+// create app object title/subtitle
+// use title/subtitle in the template
+// render template
+
+var app = {
+    title: 'Indecision App',
+    subtitle: "We've got you covered.",
+    options: ['one', 'two']
+
+    // JSX - JavaScript XML
+};var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? "Here are your options" : "No options"
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
 );
+
+var user = {
+    name: 'Logan',
+    age: 35,
+    location: 'Fort Worth'
+};
+var userName = 'Percival';
+var userAge = 36;
+var userLocation = 'Dallas';
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
+);
+
+// the else {
+//      returns undefined
+//  }
+// which can be very useful as nothing will be rendered through JSX
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
 var appRoot = document.getElementById("app");
 
+// ReactDOM.render(template, appRoot);
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,58 @@
+// JSX file that will auto-generate content via babel and output to: public/scripts/app.js
+// command line: babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
 console.log('App.js is running!');
 
+// create app object title/subtitle
+// use title/subtitle in the template
+// render template
+
+var app = {
+    title: 'Indecision App',
+    subtitle: "We've got you covered.",
+    options: ['one', 'two']
+}
+
 // JSX - JavaScript XML
-var template = <h1>Changed again?</h1>;
+var template =(
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var user = {
+    name: 'Logan',
+    age: 35,
+    location: 'Fort Worth'
+}
+var userName = 'Percival';
+var userAge = 36;
+var userLocation = 'Dallas';
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+);
+
+// the else {
+//      returns undefined
+//  }
+// which can be very useful as nothing will be rendered through JSX
+function getLocation(location) {
+    if (location) {
+        return (<p>Location: {location}</p>);
+    }
+}
+
 var appRoot = document.getElementById("app");
 
+// ReactDOM.render(template, appRoot);
 ReactDOM.render(template, appRoot);
+

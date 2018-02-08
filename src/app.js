524b41ddbced14975abff6b71f288db1ff5ef772
Logan Wood <lwood3499@gmail.com>
2018-02-07 21:28:26 -0600

    configuring webpack with babel-core babel-loader and creating .babelrc to handle presets

--- src/app.js
+++ src/app.js
@@ -1,18 +1,7 @@
-// import './utils.js';
+// install --> import --> use
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// import a single named export item, in this case, a function
-import {square, add, times3, objToExport} from './utils.js';
-
-console.log('app.js is running now');
-
-console.log(square(5));
-console.log(add(5, 10));
-console.log(times3(8));
-
-console.log(objToExport.name, 'is ', objToExport.age, ' years old.');
-
-// person.js
-import {isAdult, canDrink} from './person.js';
-console.log("I'm 17, so I'm an adult right? ", isAdult(17));
-console.log("I am indeed 35, so I can drink right? ", canDrink(35));
+const template = <p>THIS IS JSX FROM WEBPACK!!!</p>;
+ReactDOM.render(template, document.getElementById('app'));
 
--- src/utils.js
+++ src/utils.js
@@ -4,10 +4,23 @@ const square = (x) => x * x;
 const add = (a, b) => a + b;
 
 // can export an individual item:
-// export {square]};
+// export {square};
+
+
+const subtract = (a, b) => a - b;
 
 // can export multiple named items:
-export {square, add};
+// export {square, add, subtract as default};
+// YOU CAN ALSO EXPORT DEFAULT THE FOLLOWING 2 WAYS:
+    // 1. const timesNums = (a, b) => a * b;
+    //      export default timesNums;
+    //  NOTE: export default const timesNums = (a, b) => a * b; WILL NOT WORK
+    
+    // 2. export default (a, b) => a * b;
+    // NOTE: We don't really need to name it here because when you export
+    // a default, you can name it on the import side.
+
+
 
 // can export in-line with item:
 export const times3 = (x) => x * 3;
@@ -19,3 +32,9 @@ const objToExport = {
 }
 
 export {objToExport};
+
+import validator from 'validator';
+
+const oppositeOfValid = !(validator.isEmail('abc@123.com'));
+
+export default oppositeOfValid;
